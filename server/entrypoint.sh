0e9e0e83aecca90deb8a140df4240c2d74a0c646
jiseok <jiseok>
2023-05-31 22:48:45 +0900

    Window Docker prd ssh 연결 안됨 현상 해결

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+chmod 740 /root/.ssh/authorized_keys &&
+chmod 700 /root/.ssh &&
+chmod 700 /root/ &&
 nohup /usr/sbin/sshd -D -e &
-
 cd /back && gradle bootJar && java -jar build/libs/server-1.0.jar
--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-
+chmod 740 /root/.ssh/authorized_keys &&
+chmod 700 /root/.ssh &&
+chmod 700 /root/ &&
 nohup /usr/sbin/sshd -D -e &
 envsubst '$${WEB_PORT}' < /etc/nginx/conf.d/default-template > /etc/nginx/conf.d/default.conf &&  nginx -g 'daemon off;'
