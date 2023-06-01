c5871e52fc990adbe90cf9460b58963ccf60b561
정지석 <jiseok1378@gmail.com>
2023-06-01 17:52:34 +0900

    WEB 빌드 재 테스트

--- web/src/App.vue
+++ web/src/App.vue
@@ -19,7 +19,7 @@
         target="_blank"
         text
       >
-        <span class="mr-2">LOGIN</span>
+        <span class="mr-2">LOGIN ACCOUNT</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
