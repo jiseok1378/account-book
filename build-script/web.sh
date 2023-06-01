4935ea74629a1bc4afa8e7fa4827a428a25bdbcf
정지석 <jiseok1378@gmail.com>
2023-06-01 16:36:04 +0900

    Jenkins web 빌드 테스트

---
+++ build-script/web.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+PROJECT_REPO=${HOME}/account-book/web
+SSH_OPT='-o StrictHostKeyChecking=no'
+
+cd ${PROJECT_REPO}
+
+function getFrontName(){
+    echo app-web-$1
+}
+
+function getSSHName(){
+    echo root@$(getFrontName $1)
+}
+
+function build(){
+    echo '==================== BUILD WEB PROJECT [START] ===================='
+    rm -rf ./dist
+    yarn install && yarn build
+    zip -r dist.zip ./dist
+    echo '==================== BUILD WEB PROJECT [END] ===================='
+}
+
+DISTRIBUTION_INNER_VAL=0
+function distributionInner(){
+    IS_SUCC=''
+    i=0
+    while [ $? -eq 0  ]
+    do
+        if [ $i -eq 5 ]
+        then
+            break
+        fi 
+        echo checking $(getFrontName $1)...
+        IS_SUCC=$(curl -s http://$(getFrontName $1):8$1)
+        if [ ! -z "$IS_SUCC" ]
+        then
+            scp ${SSH_OPT} ./dist.zip $(getSSHName "$1"):/
+            ssh ${SSH_OPT} $(getSSHName "$1") 'unzip -qq -o /dist.zip -d / && rm /dist.zip && cp -rf /dist/* /user/share/nginx/html && rm -rf /dist'
+            echo "Success [$(getFrontName $1)]"
+            break
+        fi
+        ((i++))
+        sleep 1
+    done
+    if [ -z "$IS_SUCC" ]
+    then
+        DISTRIBUTION_INNER_VAL=-1
+        # echo 'FAILED::DISTRIBUTION'
+    fi
+}
+
+function finally(){
+    rm -rf ./dist ./dist.zip 
+}
+
+function distribution(){
+    CURRENT_IDX=0
+    for var in $@
+    do
+        if [ ! $CURRENT_IDX -eq 0 ] && [ ! $DISTRIBUTION_INNER_VAL -eq 0 ]
+        then
+            case "$DISTRIBUTION_INNER_VAL" in
+                '-1') echo "[$(getFrontName "$CURRENT_IDX")] DISTRIBUTION_FAILED::CONNECT_FAILED";;
+            esac
+            break;
+        fi
+        distributionInner "$var"
+        ((CURRENT_IDX++))
+    done 
+}
+
+
+build
+distribution '1' '2'
+finally
