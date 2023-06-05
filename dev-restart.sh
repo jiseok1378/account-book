0b9516d751372bec50412f7cc6d4cc447e76633b
정지석 <jiseok1378@gmail.com>
2023-06-05 22:34:48 +0900

    docker build 옵션 추가

--- dev-restart.sh
+++ dev-restart.sh
@@ -3,6 +3,15 @@
 APP_SERVER_NAME=app-server-dev
 APP_WEB_NAME=app-web-dev
 TARGET=$1
+IS_REBUILD=$2
+
+
+function rebuild(){
+    if [ $IS_REBUILD = 'build' ]
+    then
+        bash docker-compose-prd build
+    fi
+}
 
 function restartContainer(){
     if [ $TARGET = 'web' ] || [ $TARGET = 'w' ] 
@@ -19,6 +28,7 @@ if [ -z $TARGET ]
 then
     echo "USEAGE: $0 [web | server]"
 else
+    rebuild &&
     restartContainer
 fi 
 
