b0fd037da7edbe2e8a74ae0e644cec4a525fb455
root <root@DESKTOP-6VON38F.localdomain>
2023-06-05 22:28:46 +0900

    Server Prd 도커 빌드 수정

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,4 +3,4 @@ chmod 740 /root/.ssh/authorized_keys &&
 chmod 700 /root/.ssh &&
 chmod 700 /root/ &&
 nohup /usr/sbin/sshd -D -e &
-cd /back && gradle bootJar && java -jar build/libs/server-1.0.jar
+java -jar build/libs/server-1.0.jar
